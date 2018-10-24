3a21034ec2b2210a2aca5c7efb950b6d1052b1ad
Youssless <33607275+Youssless@users.noreply.github.com>
2018-10-24 13:40:18 +0100

    Add files via upload

---
+++ 1dca.h
@@ -0,0 +1,11 @@
+int printRow(int row[], int rowSize);
+
+int calculateCell(int lastRowLeft, int lastRowCentre, int lastRowRight, int rule[]);
+
+int calculateNextRow(int currentRow[], int rule[], int rowSize);
+
+int* decimalToBinary(int decimal);
+
+int binaryToDecimal(int rule[]);
+
+int rnd(void);
