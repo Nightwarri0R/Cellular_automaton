eac58837392f368e8d2bca70f5a17c72af0e9674
Youssless <33607275+Youssless@users.noreply.github.com>
2018-10-25 18:57:37 +0100

    Add files via upload

--- 1d_cellular_automaton.c
+++ 1d_cellular_automaton.c
@@ -2,17 +2,18 @@
 #include <stdlib.h>
 #include <time.h>
 #include <math.h>
+#include <string.h>
 #include "1dca.h"
 
 int main(void) {
 	int *rule;
-	int row[80];
-	int rowSize = 80;
+	int row[40];
+	int rowSize = 40;
 
 	for (int i=0; i<rowSize; i++)
 		row[i] = 0;
 
-	row[40] = 1;
+	row[20] = 1;
 
 	//int rule[] = {0,1,1,1,1,0,0,0};
 	//            1 2 4 8 16 32 64 128
@@ -53,6 +54,32 @@ int main(void) {
 	*/
 }
 
+int calculateCell(int lastRowLeft, int lastRowCentre, int lastRowRight, int rule[]) {
+	// eg) lastRowLeft = 1, lastRowCentre = 0, lastRowRight = 1 then if 
+	// rule[5] == 1 then 1 is returned else 0 is returned
+	if ( rule[(lastRowLeft*4)+(lastRowCentre*2)+(lastRowRight)] == 1 )
+		return 1;
+	else
+		return 0;
+}
+
+void calculateNextRow(int currentRow[], int rule[], int rowSize) {
+
+	int nextRow[rowSize];
+	// wrap around when the at the 0th position in the nextRow
+	nextRow[0] = calculateCell(currentRow[rowSize-1], currentRow[0], currentRow[1], rule);
+
+	for (int i=1; i<(rowSize-1); i++)
+		nextRow[i] = calculateCell(currentRow[i-1], currentRow[i], currentRow[i+1], rule);
+
+	// wrap around whan at the i-1 position in the nextRow
+	nextRow[rowSize-1] = calculateCell(currentRow[rowSize-2], currentRow[rowSize-1], currentRow[0],rule);
+
+	// copy the memory of nextRow array to the currentRow array using the memcpy function from <string.h> library
+	memcpy(currentRow, nextRow, sizeof(nextRow));
+
+}
+
 void printRow(int row[], int rowSize) {
 	// prints a white block if the arr contains 1. blank for 0
 	for (int i=0; i<rowSize; i++)
@@ -60,32 +87,6 @@ void printRow(int row[], int rowSize) {
 			printf("\u2588");
 		else
 			printf(" ");
-}
-
-int calculateCell(int lastRowLeft, int lastRowCentre, int lastRowRight, int rule[]) {
-
-	if ( rule[(lastRowLeft*4)+(lastRowCentre*2)+(lastRowRight)] == 1 )
-		return 1;
-	else
-		return 0;
-}
-
-int calculateNextRow(int currentRow[], int rule[], int rowSize) {
-
-	int nextRow[rowSize];
-
-	nextRow[0] = calculateCell(currentRow[rowSize-2], currentRow[rowSize-1], 0, rule);
-
-	for (int i=1; i<(rowSize-1); i++)
-		nextRow[i] = calculateCell(currentRow[i-1], currentRow[i], currentRow[i+1], rule);
-
-	nextRow[rowSize-1] = calculateCell(nextRow[0], nextRow[1], 0, rule);
-
-	for (int i=0; i<rowSize; i++)
-		currentRow[i] = nextRow[i];
-
-	return 0;
-
 }
 
 int* decimalToBinary(int decimal) { 
@@ -104,7 +105,8 @@ int* decimalToBinary(int decimal) {
         binary[bit] = modulus; 
         bit++;
     }
-	
+
+	// returns array address
     return binary;
 }
 
@@ -125,7 +127,9 @@ int rnd(void) {
 	int max = 255;
 	time_t t;
 
-	srand((unsigned) time(&t));
+	// genterates a random number based on the current date and time
+	// srand() is used to initialse random number generator
+	srand((unsigned) time(&t)); 
 	random = rand() % max;
 
 	return random;
